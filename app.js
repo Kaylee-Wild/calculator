47fb8e3f80a80f4ff3249ea342739105a1708762
Kaylee Wild <wildkaylee13@gmail.com>
2024-02-05 20:01:24 -0700

    buttons are displaying in display their value but replace the last

--- app.js
+++ app.js
@@ -3,21 +3,36 @@ window.onload = function() {
 
 
 //instead of finding each number button by each specific ID,
-//use querySelectorAll() to search for all number buttons with
+//use maybe I use querySelectorAll() to search for all number buttons with
 //the same CSS class to put them in an array
 //then have the function call the items that querySelectorAll() has
 //based on the number button that is clicked
 
 
-const numberBtn = document.querySelectorAll("#number");
+/*
+const display = document.getElementById("display");
+
+
+const numberOneBtn = document.getElementById("one");
+const numberTwoBtn = document.getElementById("two");
+const numberThreeBtn = document.getElementById("three");
 
 function print() {
+
+
     console.log("you pressed a number");
 }
 
 
-//currently giving error "numberBtn.addEventListener is not a function"
-numberBtn.addEventListener('click',print);
+numberOneBtn.addEventListener('click',print);
+*/
+
+const displayContainer = document.getElementById('display')
+document.querySelectorAll('button').forEach((item) => {
+  item.addEventListener('click', (e) => {
+    displayContainer.innerHTML = e.target.textContent.trim();
+  })
+})
 
 
 
