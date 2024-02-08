5dc54b5472d1cf6c0f360cddd825d5c14782f418
Kaylee Wild <wildkaylee13@gmail.com>
2024-02-07 20:16:45 -0700

    removed old comements and added new ones to list what should happen when any other button is pressed

--- app.js
+++ app.js
@@ -1,40 +1,82 @@
 window.onload = function() {
 
 
-
-//instead of finding each number button by each specific ID,
-//use maybe I use querySelectorAll() to search for all number buttons with
-//the same CSS class to put them in an array
-//then have the function call the items that querySelectorAll() has
-//based on the number button that is clicked
-
-
-/*
-const display = document.getElementById("display");
-
-
-const numberOneBtn = document.getElementById("one");
-const numberTwoBtn = document.getElementById("two");
-const numberThreeBtn = document.getElementById("three");
-
-function print() {
-
-
-    console.log("you pressed a number");
-}
-
-
-numberOneBtn.addEventListener('click',print);
-*/
-
+/*when button is pressed, value is printed in display
+need to get the value to be saved to the full equation*/
 const displayContainer = document.getElementById('display')
 document.querySelectorAll('button').forEach((item) => {
   item.addEventListener('click', (e) => {
-    displayContainer.innerHTML = e.target.textContent.trim();
+    displayContainer.innerHTML += e.target.textContent.trim();
   })
 })
 
 
+
+
+
+/*when Del is pressed, delete the latest character in display*/
+
+
+
+
+
+
+
+/*when +, -, X, /, or % button is pressed,
+clear the display and print the operator in the display
+
+when the next constant is entered, clear the operator from the display
+and save the operator to the full equation
+
+The constant entered before an operator needs to be saved
+so it can be recalled to get the full equation*/
+
+
+
+
+
+
+
+/*when C is pressed, clear the full equation and display*/
+
+
+
+
+
+
+
+
+/*when +/- is pressed, switch the sign of the constant*/
+
+
+
+
+
+
+
+/*when () is pressed, will be a bit trickier
+
+left parenthesis needs to be printed and added the full equation first
+but it needs to only be on the left side of the constant
+
+right parenthesis needs to be printed and added to the full equation
+and only be on the right side of the constant
+
+will likely need to add in some way to move left and right in the
+display to add more than one parenthesis inside each other and so on
+
+e.g. ((85x2)+3)
+*/
+
+
+
+
+
+
+
+/*when = is pressed, need to have it NOT print but then instead just
+calculate the results of the full equation and print the results
+on the display*/
 
 
 
